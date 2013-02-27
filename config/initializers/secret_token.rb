5cae07d5dd612a45264adbb8104acf8427632f1b
JHP-tido <JHP-tido@hotmail.com>
2013-02-27 12:45:43 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'd74b43f6f5798b9d9c405f2a8630683238f01cefde47b55d9c84cc376161b0154fcc0ceef9bb718b1d15c88ba555832280eea8db92dbafa31ac98c7ca0e0b5ac'
